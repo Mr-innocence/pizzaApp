da63656930b2f604775142cb65d8c8d35efeb0da
Mr-innocence <bellali1019@gmail.com>
2019-07-14 15:49:58 +1000

    complete

--- pizza-attempt/index.js
+++ pizza-attempt/index.js
@@ -1,3 +1,100 @@
+let state = {
+    toppings: [
+        {
+            name: 'Anchovy',
+            imgSrc: './assets/toppings/anchovy.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Bacon',
+            imgSrc: './assets/toppings/bacon.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Basil',
+            imgSrc: './assets/toppings/basil.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Chili',
+            imgSrc: './assets/toppings/chili.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Mozzarella',
+            imgSrc: './assets/toppings/mozzarella.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Mushroom',
+            imgSrc: './assets/toppings/mushroom.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Olive',
+            imgSrc: './assets/toppings/olive.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Onion',
+            imgSrc: './assets/toppings/onion.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Pepper',
+            imgSrc: './assets/toppings/pepper.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Pepperoni',
+            imgSrc: './assets/toppings/pepperoni.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Peppers',
+            imgSrc: './assets/toppings/peppers.svg',
+            price: 0.99,
+        },
+        {
+            name: 'Sweetcorn',
+            imgSrc: './assets/toppings/sweetcorn.svg',
+            price: 0.99,
+        },
+        ], 
+    selectedToppingNames: ['Pepper'],
+}
+
+function onToppingDivClick(topping){
+    const { name } = topping;
+    const { selectedToppingNames } = state;
+    const activated = selectedToppingNames.includes(name);
+
+    function deactive()
+    {
+        const newSelectedToppingNames = selectedToppingNames.filter(thisName => thisName !== name);
+        state = {
+            ...state,
+            selectedToppingNames: newSelectedToppingNames
+        };
+        render();
+    }
+
+    function activate(){
+        const newSelectedToppingNames = [...selectedToppingNames, name];
+        state = {
+            ...state,
+            selectedToppingNames: newSelectedToppingNames
+        };
+        render();            
+    }
+
+    if(activated){
+        deactive();
+        return;           
+    }
+    activate();
+}
+
 function renderTopping({ topping, activated, onClick}){
     const {imgSrc, name} = topping;
     const toppingDiv = document.createElement('div');
@@ -26,103 +123,7 @@ function renderTopping({ topping, activated, onClick}){
     return toppingDiv;
 }
 
-function renderToppings(){
-    let state = {
-        toppings: [
-            {
-                name: 'Anchovy',
-                imgSrc: './assets/toppings/anchovy.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Bacon',
-                imgSrc: './assets/toppings/bacon.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Basil',
-                imgSrc: './assets/toppings/basil.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Chili',
-                imgSrc: './assets/toppings/chili.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Mozzarella',
-                imgSrc: './assets/toppings/mozzarella.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Mushroom',
-                imgSrc: './assets/toppings/mushroom.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Olive',
-                imgSrc: './assets/toppings/olive.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Onion',
-                imgSrc: './assets/toppings/onion.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Pepper',
-                imgSrc: './assets/toppings/pepper.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Pepperoni',
-                imgSrc: './assets/toppings/pepperoni.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Peppers',
-                imgSrc: './assets/toppings/peppers.svg',
-                price: 0.99,
-            },
-            {
-                name: 'Sweetcorn',
-                imgSrc: './assets/toppings/sweetcorn.svg',
-                price: 0.99,
-            },
-            ], 
-        selectedToppingNames: ['Pepper'],
-    }
-
-    function onToppingDivClick(topping){
-        const { name } = topping;
-        const activated = selectedToppingNames.includes(name);
-
-        function deactive()
-        {
-            const newSelectedToppingNames = selectedToppingNames.filter(thisName => thisName !== name);
-            state = {
-                ...state,
-                selectedToppingNames: newSelectedToppingNames
-            };
-            render();
-        }
-
-        function activate(){
-            const newSelectedToppingNames = [...selectedToppingNames, name];
-            state = {
-                ...state,
-                selectedToppingNames: newSelectedToppingNames
-            };
-            render();            
-        }
-
-        if(activated){
-            deactive();
-            return;           
-        }
-        activate();
-    }
-
+function renderToppings(state){
     const { toppings, selectedToppingNames } = state;
     
     const toppingsDiv = document.createElement('div');
@@ -147,23 +148,23 @@ function renderTitle({title}) {
     return titleH2;
 }
 
-function renderChooseYourToppings(){
+function renderChooseYourToppings(state){
     const chooseYourToppingsSection = document.createElement('section');
     chooseYourToppingsSection.classList.add('section', 'toppings');
 
     const chooseYourToppingsTitle = renderTitle({title: 'choose your topping'});
-    const chooseYourToppings = renderToppings();
+    const chooseYourToppings = renderToppings(state);
 
     chooseYourToppingsSection.append(chooseYourToppingsTitle, chooseYourToppings);
 
     return chooseYourToppingsSection;
 }
 
-function renderPizzaCreator(){
+function renderPizzaCreator(state){
     const pizzaCreatorDiv = document.createElement('div');
     pizzaCreatorDiv.classList.add('pizza-creator-app');
 
-    const chooseYourToppings = renderChooseYourToppings();
+    const chooseYourToppings = renderChooseYourToppings(state);
 
     pizzaCreatorDiv.append(chooseYourToppings);
 
@@ -172,7 +173,7 @@ function renderPizzaCreator(){
 
 function render(){
     const root = document.querySelector('#root');
-    const pizzaCreator = renderPizzaCreator();
+    const pizzaCreator = renderPizzaCreator(state);
     root.innerHTML = null;
     root.append(pizzaCreator);
 }
